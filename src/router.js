80885a88171d686ff85f27359901d0c97122c9a5
kongjian <kongjian0801@163.com>
2025-04-10 11:12:32 +0800

    自动发布测试

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const ENV = process.env.NODE_ENV
 router.get('/api/test', async (ctx, next) => {
     ctx.body = {
         errno: 0,
-        msg: `koa2 test OK , ENV: ${ENV}`
+        msg: `自动发布测试 OK , ENV: ${ENV}`
     }
 })
 
